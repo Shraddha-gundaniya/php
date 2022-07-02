20447dd7fddeb879a30dd0dc2904d3f7318665f5
Shraddha Patel <91522775+Shraddha-gundaniya@users.noreply.github.com>
2022-07-02 21:41:25 +0530

    Add files via upload

--- process.php
+++ process.php
@@ -1,19 +1,15 @@
 <?php
-//$_GET['controlname'] $_POST['controlname']
-$txt1 = $_GET['txt1'];
-$txt2 = $_GET['txt2'];
 
-echo "My Name is $txt1 and Moblie Nubmer is $txt2 ";
+$Name =$_POST['txt1'];
+$Gender=$_POST['txt2'];
 
-echo "<table border='1'>";
-for($i=0;$i<$txt2;$i++){
-    echo "<tr>";
-    if($i % 2 == 0){
-        echo "<td style ='color:blue;'> $i </td>";
-    }else{
-        echo "<td style='color:red;'> $i </td>";
-    }
-    echo "</tr>";
+echo " My name is $Name and Gender is $Gender";
+
+$connection = mysqli_connect("localhost","root","","covid19");
+$query = mysqli_query($connection , "insert into tbl_student(sd_name,sd_gender) values('$Name','$Gender')") or die("Error".mysqli_error($connection));
+
+if($query)
+{
+    echo "<script>alert('Record Added');</script>";
 }
-echo "</table>";
 ?>
